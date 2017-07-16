a013712422ab8dc6ea812fbeddd3221b144936c1
Andres Giacosa <aagiacosa@gmail.com>
2017-07-15 21:31:39 -0300

    Se agrega playerRoutes

--- app.js
+++ app.js
@@ -67,18 +67,3 @@ mongoose.connect(configDB.url, function(error) {
 
   	res.redirect('/');
 });*/
-
-
-//API routes
-/*var player = express.Router();
-
-player.route('/playerController')
-    .get(PlayerController.findAllPlayers)
-    .post(PlayerController.addPlayer);
-
-player.route('/playerController/:id')
-    .get(PlayerController.findById)
-    .put(PlayerController.updatePlayer)
-    .delete(PlayerController.deletePlayer);
-
-app.use('/api', player)*/
---
+++ server/routes/playerRoutes.js
@@ -0,0 +1,15 @@
+var player = express.Router();
+
+module.exports = function(app) {
+
+	player.route('/playerController')
+	.get(PlayerController.findAllPlayers)
+	.post(PlayerController.addPlayer);
+
+	player.route('/playerController/:id')
+	.get(PlayerController.findById)
+	.put(PlayerController.updatePlayer)
+	.delete(PlayerController.deletePlayer);
+
+	app.use('/api', player)
+};
